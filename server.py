af2c6fb428e13879b0feddb869f74116026d3ca3
sayinbu <sayinbu@itu.edu.tr>
2015-10-29 00:54:30 +0200

    Update server.py
    Trying to fix bluemix error

--- server.py
+++ server.py
@@ -29,9 +29,5 @@ def statistics_page():
 
 
 if __name__ == '__main__':
-    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
-    if VCAP_APP_PORT is not None:
-        port, debug = int(VCAP_APP_PORT), False
-    else:
-        port, debug = 5000, True
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
+    app.run(host='0.0.0.0', port=int(PORT))
