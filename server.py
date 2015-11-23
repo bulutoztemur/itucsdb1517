048759851051aae9c5f4fb9ce9a35aea24b4b80b
dalboye <dalboye@itu.edu.tr>
2015-11-23 20:15:03 +0200

    Deleted unnecessary codes

--- server.py
+++ server.py
@@ -1,25 +1,13 @@
 import datetime
 import os
 import json
-import os
 import re
 
 from flask import render_template
 from config import app
-from classes.model_config import dsn
 import team_views
 import player_views
 import statistics_views
-
-def get_elephantsql_dsn(vcap_services):
-    """Returns the data source name for ElephantSQL."""
-    parsed = json.loads(vcap_services)
-    uri = parsed["elephantsql"][0]["credentials"]["uri"]
-    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
-    user, password, host, _, port, dbname = match.groups()
-    dsn = """user='{}' password='{}' host='{}' port={}
-             dbname='{}'""".format(user, password, host, port, dbname)
-    return dsn
 
 @app.route('/')
 def home_page():
@@ -37,15 +25,4 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
-        
-    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
-    if VCAP_SERVICES is not None:
-        dsn=get_elephantsql_dsn(VCAP_SERVICES)
-        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
-    else:
-        dsn="""user='vagrant' password='vagrant'
-                               host='localhost' port=54321 dbname='itucsdb'"""
-        app.config['dsn'] = """user='vagrant' password='vagrant'
-                               host='localhost' port=54321 dbname='itucsdb'"""
-
     app.run(host='0.0.0.0', port=port, debug=debug)
