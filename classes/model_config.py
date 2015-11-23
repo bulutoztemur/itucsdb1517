54ff3a59d4b843ca25dc7b5624cc1a425b319b1b
sayinbu <sayinbu@itu.edu.tr>
2015-11-23 15:02:13 +0200

    Trying to configure ElephantSQL.

--- classes/model_config.py
+++ classes/model_config.py
@@ -1,3 +1,6 @@
+import os
+
+VCAP_SERVICES = os.getenv('VCAP_SERVICES')
 user="vagrant"
 password="vagrant"
 host="127.0.0.1"
@@ -5,4 +8,7 @@ port="54321"
 dbname="itucsdb"
 connection=None
 
-dsn = """user={0} password={1} host={2} port={3} dbname={4}""".format(user,password,host,port,dbname)
+if VCAP_SERVICES is not None:
+    dsn = get_elephantsql_dsn(VCAP_SERVICES)
+else:
+    dsn = """user={0} password={1} host={2} port={3} dbname={4}""".format(user,password,host,port,dbname)
