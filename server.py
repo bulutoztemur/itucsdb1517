2b94db49eeecdac26b07ca0f9177597a5dba1238
dalboye <dalboye@itu.edu.tr>
2015-12-01 06:22:12 +0200

    Database initialization error is corrected and Debug mode is off.

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ def login_page():
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
-        port, debug = int(VCAP_APP_PORT), True
+        port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
     app.run(host='0.0.0.0', port=port, debug=debug)
