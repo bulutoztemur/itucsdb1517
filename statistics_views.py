28298c5f5b318c2261f5373e63e9aad9d5359a10
bulutoztemur <oztemur@itu.edu.tr>
2015-12-02 17:17:25 +0200

    Statistic table are added on statistics page

--- statistics_views.py
+++ statistics_views.py
@@ -1,11 +1,18 @@
 import datetime
 
 from flask import render_template
+from flask import url_for
+from flask import redirect
+from flask import request
 
 from config import app
+from classes.statistic import Statistic
+from classes.operations.statistic_operations import statistic_operations
 
-
-@app.route('/statistics')
+@app.route('/statistics', methods=['GET','POST'])
 def statistics_page():
-    now = datetime.datetime.now()
-    return render_template('statistics.html', current_time=now.ctime())
+    if request.method == 'GET':
+        store = statistic_operations()
+        statistics=store.get_statistics()
+        now = datetime.datetime.now()
+        return render_template('statistics.html', statistics=statistics, current_time=now.ctime())
