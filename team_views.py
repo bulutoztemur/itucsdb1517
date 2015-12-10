43397678efef37510870a7ce0d02d4c7ee6c82d2
dalboye <dalboye@itu.edu.tr>
2015-12-10 15:45:41 +0200

    teams url is corrected

--- team_views.py
+++ team_views.py
@@ -9,10 +9,11 @@ from config import app
 from classes.team import Team
 from classes.operations.team_operations import team_operations
 
-@app.route('/team', methods=['GET','POST'])
-def team_page():
+@app.route('/teams', methods=['GET','POST'])
+def teams_page():
     if request.method == 'GET':
         store = team_operations()
         teams=store.get_teams()
         now = datetime.datetime.now()
         return render_template('team.html', teams=teams, current_time=now.ctime())
+
